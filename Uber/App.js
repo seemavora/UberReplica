153036a1c3e545009f00faccc7d0560af53fd650
seemavora <seema.s.vora@gmail.com>
2022-06-14 17:56:29 -0700

    Just setting up basic repo

---
+++ Uber/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+// Set up redux
+// 
+export default function App() {
+  return (
+    <Provider>
+    <View style={styles.container}> 
+      <Text>Let's build Twitter!</Text>
+    </View>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
