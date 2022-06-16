9ae59bb0b1565b26e19b61249eeeeeb428680512
seemavora <seema.s.vora@gmail.com>
2022-06-15 18:34:06 -0700

    Adds Recent Changes on CS for Homepage

--- Uber/components/NavOptions.js
+++ Uber/components/NavOptions.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { FlatList, Text, TouchableOpacity, View, Image } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+import {Icon} from 'react-native-elements';
 
 const data = [
     {
@@ -23,12 +25,20 @@ const NavOptions = () => {
             horizontal
             keyExtractor={(item) => item.id} // key allows the build to know what item you are rendering, makes things more efficient
             renderItem={({ item }) => (
-                <TouchableOpacity> 
+                <TouchableOpacity
+                style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}> 
                     {/* allows a chance in bahavior when clicked on */}
                     <View>
                         <Image
                         style={{width:120, height:120, resizeMode:'contain'}}
                         source = {{uri: item.image}}/>
+                        <Text style ={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                        <Icon 
+                        style = {tw`p-2 bg-black rounded-full w-10 mt-4`}
+                        name = "arrowright"
+                        color = "white"
+                        type='antdesign'
+                        />
                     </View>
                 </TouchableOpacity>
             )}
